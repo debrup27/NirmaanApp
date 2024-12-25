ae0e00b110d8ee297e57d963123b1afe6a74c9fe
sagnik-tech56 <sagniksinha.2004@gmail.com>
2024-12-25 14:11:45 +0530

    Login and SignUp Page added

---
+++ app/index.jsx
@@ -0,0 +1,18 @@
+/* Made by Debrup Sengupta for queries debrupsengupta289@gmail.com */
+//Comment for bug fixes & debugging or new functionalities added
+import { Button, View, Text } from 'react-native'
+import React from 'react'
+import { useRouter } from 'expo-router';
+import ScreenWrapper from '../components/ScreenWrapper';
+
+const index = () => {
+    const router = useRouter();
+  return (
+    <ScreenWrapper>
+      <Text>index</Text>
+      <Button title="welcome" onPress={()=>router.push('welcome')} />
+    </ScreenWrapper>
+  )
+}
+
+export default index
